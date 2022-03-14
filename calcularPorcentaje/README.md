e4cea2f7c45d75815052dd7fa27a8f45827bab33
Rafael Malpica <rafaelmalpica888@gmail.com>
2022-03-14 16:16:20 -0400

    Añadiendo formualas de porcentaje

--- calcularPorcentaje/README.md
+++ calcularPorcentaje/README.md
@@ -2,3 +2,8 @@
 Curso práctico de JavaScript
 
 ## Taller #2: Precios y Descuentos
+
+- Primer paso: Definir la fórmula
+- Segundo paso: Implementar la fórmula en JavaScript
+- Tercer Paso: Crear funciones
+- Cuarto paso: Interactuar con HTML
---
+++ calcularPorcentaje/app.js
@@ -0,0 +1,11 @@
+const precioOriginal = 100;
+const descuento = 18;
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento
+}
+
+
+

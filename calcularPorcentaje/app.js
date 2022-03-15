46a699d48561c43a3b0219eee7473a20151cdf1c
Rafael Malpica <rafaelmalpica888@gmail.com>
2022-03-15 16:06:36 -0400

    Taller #2 Completado

--- calcularPorcentaje/app.js
+++ calcularPorcentaje/app.js
@@ -1,11 +1,39 @@
-const precioOriginal = 100;
-const descuento = 18;
-
 function calcularPrecioConDescuento(precio, descuento) {
-    const porcentajePrecioConDescuento = 100 - descuento;
-    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
-    return precioConDescuento
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  return precioConDescuento;
 }
 
+const coupons = [
+  "JuanDC_es_Batman",
+  "pero_no_le_digas_a_nadie",
+  "es_un_secreto",
+];
 
-
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+  
+    let descuento;
+  
+    switch(couponValue) {
+      case coupons[0]: 
+        descuento = 15;
+      break;
+      case coupons[1]: 
+        descuento = 30;
+      break;
+      case coupons[2]: 
+        descuento = 25;
+      break;
+    }
+  
+  
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+  
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  }

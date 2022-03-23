e7f19988ab3328688d549e2a92c10ce7d7f01a31
Rafael Malpica <rafaelmalpica888@gmail.com>
2022-03-23 09:41:58 -0400

    Taller #3 listo

--- PromedioMedianaModa/moda.js
+++ PromedioMedianaModa/moda.js
@@ -9,3 +9,12 @@ lista1.map(function (elemento) {
     lista1Count[elemento] = 1;
   }
 });
+
+const lista1Array = Object.entries(lista1Count).sort(function (
+  elementoA,
+  elementoB
+) {
+  return elementoA[1] - elementoB[1];
+});
+
+const moda = lista1Array[lista1Array.length - 1];
